dc26fccb726821bf5c3d6fb28bda9f64886b749a
shahil-leo <shahilcode@gmail.com>
2022-07-22 23:35:43 +0530

    Added the .env file to add all the secrets

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express =require ("express")
 const app=express();
 const mongoose=require("mongoose")
+const dotenv=require("dotenv")
+
+dotenv.config();
+
+// this is for the connection of the mongodb with the vscode and there is a some arrow function we need to understand 
+// if it works then it will be DB connection successfull if not then it will be err
+mongoose.connect( process.env.MONGO_URL)
+.then(()=>console.log("DB Connection Successfull"))
+.catch((err)=>{
+    console.log(err);
+});
+
+// app.get("/api/test",()=>{
+//     console.log("test is successfull")
+// })
 
 
 
-app.listen(5000, ()=>{
+app.listen( 5000, ()=>{
     console.log("backend server is running")
 })
