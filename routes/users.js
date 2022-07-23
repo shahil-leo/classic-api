0e233c6c5b177e21857ba8dbe37427562e86cd26
shahil-leo <shahilcode@gmail.com>
2022-07-23 14:57:36 +0530

    we added the routes for the users cart and products admin etc..

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,5 @@
 const router=require("express").Router();
 
-// when we type the localhost:then the address of the index.js the nthe usertest 
-// it will give you the you are a killaid test
-router.get("/usertest",(req,res)=>{
-    res.send("You are a killadi")
-})
 
-router.post("/userposttest",(req,res)=>{
-    const username=req.body.username
-    console.log(username)
-    res.send("Your username is "+ username)
-})
 
-module.exports=router;
+module.exports=router; 
